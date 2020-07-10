31d7e8e8d89c40e004f528c09140c37dc4782591
SayaliPatil <sayalipatil1505@gmail.com>
2020-07-10 00:22:34 -0400

    My First commit

---
+++ Admin Pannel/index.php
@@ -0,0 +1,257 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<!-- Copied from http://rn53themes.net/themes/demo/lava-admin/index.html by Cyotek WebCopy 1.7.0.600, Monday, September 9, 2019, 6:42:26 PM -->
+
+<head>
+    <title> Admin Panel </title>
+   <?php include_once("css_file.php");  ?>
+</head>
+
+<body>
+    <!--== MAIN CONTRAINER ==-->
+  <?php include_once("header.php") ?>
+    <!--== BODY CONTNAINER ==-->
+    <div class="container-fluid sb2">
+        
+            <?php include_once("sidebar.php") ?>
+            <!--== BODY INNER CONTAINER ==-->
+            <div class="sb2-2">
+                <!--== breadcrumbs ==-->
+                <div class="sb2-2-2">
+                    <ul>
+                        <li><a href="index.html"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+                        </li>
+                        <li class="active-bre"><a href="#"> Dashboard</a>
+                        </li>
+                        <li class="page-back"><a href="index.html"><i class="fa fa-backward" aria-hidden="true"></i> Back</a>
+                        </li>
+                    </ul>
+                </div>
+                   <?php
+
+            include_once("config.php");
+
+            $query = "SELECT * FROM tbl_city ";  
+                
+                if($result = mysqli_query($conn,$query))
+                {  
+                            $rowcount=mysqli_num_rows($result);
+                }
+                mysqli_free_result($result);
+
+                  ?>
+           
+                <!--== DASHBOARD INFO ==-->
+                <div class="ad-v2-hom-info">
+					<div class="ad-v2-hom-info-inn">
+						<ul>
+							<li>
+								<div class="ad-hom-box ad-hom-box-1">
+									<span class="ad-hom-col-com ad-hom-col-1"><i class="fa fa-bar-chart"></i></span>
+									<div class="ad-hom-view-com">
+									<p><i class="fa  fa-arrow-up up"></i>Total Cities</p>
+									<h3><?php echo $rowcount; ?></h3>
+									</div>
+								</div>
+							</li>
+							<li>
+            <?php
+
+            include_once("config.php");
+
+            $query = "SELECT * FROM tbl_attractions ";  
+                
+                if($result = mysqli_query($conn,$query))
+                {  
+                            $rowcount=mysqli_num_rows($result);
+                }
+                mysqli_free_result($result);
+
+                  ?>
+           
+								<div class="ad-hom-box ad-hom-box-2">
+									<span class="ad-hom-col-com ad-hom-col-2"><i class="fa fa-usd"></i></span>
+									<div class="ad-hom-view-com">
+									<p><i class="fa  fa-arrow-up up"></i>Attraction</p>
+									<h3><?php echo $rowcount; ?></h3>
+									</div>
+								</div>
+							</li>
+<?php
+
+            include_once("config.php");
+
+            $query = "SELECT * FROM tbl_hospital ";  
+                
+                if($result = mysqli_query($conn,$query))
+                {  
+                            $rowcount=mysqli_num_rows($result);
+                }
+                mysqli_free_result($result);
+
+                  ?>
+           
+                            <li>
+                                <div class="ad-hom-box ad-hom-box-1">
+                                    <span class="ad-hom-col-com ad-hom-col-1"><i class="fa fa-bar-chart"></i></span>
+                                    <div class="ad-hom-view-com">
+                                    <p><i class="fa  fa-arrow-up up"></i>Total Hospitals</p>
+                                    <h3><?php echo $rowcount; ?></h3>
+                                    </div>
+                                </div>
+                            </li>
+
+<?php
+
+            include_once("config.php");
+
+            $query = "SELECT * FROM tbl_restaurant ";  
+                
+                if($result = mysqli_query($conn,$query))
+                {  
+                            $rowcount=mysqli_num_rows($result);
+                }
+                mysqli_free_result($result);
+
+                  ?>
+           
+                            <li>
+                                <div class="ad-hom-box ad-hom-box-1">
+                                    <span class="ad-hom-col-com ad-hom-col-1"><i class="fa fa-bar-chart"></i></span>
+                                    <div class="ad-hom-view-com">
+                                    <p><i class="fa  fa-arrow-up up"></i>Restaurants</p>
+                                    <h3><?php echo $rowcount; ?></h3>
+                                    </div>
+                                </div>
+                            </li>
+
+
+<?php
+
+            include_once("config.php");
+
+            $query = "SELECT * FROM tbl_college ";  
+                
+                if($result = mysqli_query($conn,$query))
+                {  
+                            $rowcount=mysqli_num_rows($result);
+                }
+                mysqli_free_result($result);
+
+                  ?>
+           
+                            <li>
+                                <div class="ad-hom-box ad-hom-box-1">
+                                    <span class="ad-hom-col-com ad-hom-col-1"><i class="fa fa-bar-chart"></i></span>
+                                    <div class="ad-hom-view-com">
+                                    <p><i class="fa  fa-arrow-up up"></i>Total Colleges</p>
+                                    <h3><?php echo $rowcount; ?></h3>
+                                    </div>
+                                </div>
+                            </li>
+
+
+                            <?php
+
+            include_once("config.php");
+
+            $query = "SELECT * FROM tbl_registration ";  
+                
+                if($result = mysqli_query($conn,$query))
+                {  
+                            $rowcount=mysqli_num_rows($result);
+                }
+                mysqli_free_result($result);
+
+                  ?>
+           
+							<li>
+								<div class="ad-hom-box ad-hom-box-3">
+									<span class="ad-hom-col-com ad-hom-col-3"><i class="fa fa-address-card-o"></i></span>
+									<div class="ad-hom-view-com">
+									<p><i class="fa  fa-arrow-up up"></i> Users</p>
+									<h3><?php echo $rowcount; ?></h3>
+									</div>
+								</div>
+							</li>
+                              <?php
+
+            include_once("config.php");
+
+            $query = "SELECT * FROM tbl_enquiry ";  
+                
+                if($result = mysqli_query($conn,$query))
+                {  
+                            $rowcount=mysqli_num_rows($result);
+                }
+                mysqli_free_result($result);
+
+                  ?>
+							<li>
+								<div class="ad-hom-box ad-hom-box-4">
+									<span class="ad-hom-col-com ad-hom-col-4"><i class="fa fa-envelope-open-o"></i></span>
+									<div class="ad-hom-view-com">
+									<p><i class="fa  fa-arrow-up up"></i> Enquiry</p>
+									<h3><?php echo $rowcount; ?></h3>
+									</div>
+								</div>
+							</li>
+                             <?php
+
+            include_once("config.php");
+
+            $query = "SELECT * FROM tbl_feedback ";  
+                
+                if($result = mysqli_query($conn,$query))
+                {  
+                            $rowcount=mysqli_num_rows($result);
+                }
+                mysqli_free_result($result);
+
+                  ?>
+                            <li>
+                                <div class="ad-hom-box ad-hom-box-4">
+                                    <span class="ad-hom-col-com ad-hom-col-4"><i class="fa fa-envelope-open-o"></i></span>
+                                    <div class="ad-hom-view-com">
+                                    <p><i class="fa  fa-arrow-up up"></i>Feedback</p>
+                                    <h3><?php echo $rowcount; ?></h3>
+                                    </div>
+                                </div>
+                            </li>
+						</ul>
+					</div>
+                </div>
+               
+            
+
+        </div>
+    </div>
+
+    <!--== BOTTOM FLOAT ICON ==-->
+    <section>
+        <div class="fixed-action-btn vertical">
+            <a class="btn-floating btn-large red pulse">
+                <i class="large material-icons">mode_edit</i>
+            </a>
+            <ul>
+                <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a>
+                </li>
+                <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a>
+                </li>
+                <li><a class="btn-floating green"><i class="material-icons">publish</i></a>
+                </li>
+                <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <!--======== SCRIPT FILES =========-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+<!-- Copied from http://rn53themes.net/themes/demo/lava-admin/index.html by Cyotek WebCopy 1.7.0.600, Monday, September 9, 2019, 6:42:26 PM -->
+</html>
---
+++ Admin Pannel/sidebar.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title> Admin Panel </title>
+   <?php include_once("css_file.php");  ?>
+</head>
+
+
+
+ <!--== USER INFO ==-->
+            <div class="sb2-1" style="height: 100%;">
+               
+            
+    <div class="sb2-12" >
+                    <ul>
+                        <li><img src="images/placeholder.jpg" alt="">
+                        </li>
+                        <li>
+                            <h5>ADMIN <span> </span></h5>
+                        </li>
+                        <li></li>
+                    </ul>
+                </div>
+                <!--== LEFT MENU ==-->
+                <div class="sb2-13">
+                    <ul class="collapsible" data-collapsible="accordion" >
+                        <li><a href="index.php" class="menu-active"><i class="fa fa-bar-chart" aria-hidden="true"></i> Dashboard</a>
+                        </li>
+                        <li><a href="javascript:void(0)" class="collapsible-header"><i class="fa fa-list-ul" aria-hidden="true"></i> Add </a>
+                            <div class="collapsible-body left-sub-menu">
+                                <ul>
+                                    <li><a href="all_cities.php">All Cities</a>
+                                    </li>
+                                    <li><a href="add_info.php">Add New City</a>
+                                    </li>
+                                   
+                                <!--    <li><a href="listing-cat-all.html">All listing Categories</a>
+                                    </li>
+                                    <li><a href="listing-cat-add.html">Add listing Categories</a>
+                                    </li>-->
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a href="javascript:void(0)" class="collapsible-header"><i class="fa fa-list-ul" aria-hidden="true"></i> Update </a>
+                            <div class="collapsible-body left-sub-menu">
+                                <ul>
+                                    <li><a href="all_cities.php">All Cities</a>
+                                    </li>
+                                    <li><a href="all_hospitals.php">All Hospitals</a>
+                                    </li>
+                                   <li><a href="all_colleges.php">All Colleges</a>
+                                    </li>
+                                   <li><a href="all_restaurants.php">All Restaurants</a>
+                                    </li>
+                                   <li><a href="all_shopping_places.php">All Shopping Places</a>
+                                    </li>
+                                   <li><a href="all_attractions.php">All Attraction</a>
+                                    </li>
+                                   
+                                <!--    <li><a href="listing-cat-all.html">All listing Categories</a>
+                                    </li>
+                                    <li><a href="listing-cat-add.html">Add listing Categories</a>
+                                    </li>-->
+                                </ul>
+                            </div>
+                        </li>
+                        
+                         <li><a href="javascript:void(0)" class="collapsible-header"><i class="fa fa-list-ul" aria-hidden="true"></i> Add Types of Places</a>
+                            <div class="collapsible-body left-sub-menu">
+                                <ul>
+                                    <li><a href="view_type_of_places.php">All Types Of Places</a>
+                                    </li>
+                                    <li><a href="add_type_of_places.php">Add New Type of places</a>
+                                    </li>
+                                   
+                                <!--    <li><a href="listing-cat-all.html">All listing Categories</a>
+                                    </li>
+                                    <li><a href="listing-cat-add.html">Add listing Categories</a>
+                                    </li>-->
+                                </ul>
+                            </div>
+                        </li>
+                       
+                        <li><a href="javascript:void(0)" class="collapsible-header"><i class="fa fa-user" aria-hidden="true"></i> Users</a>
+                            <div class="collapsible-body left-sub-menu">
+                                <ul>
+                                    <li><a href="users.php">All Users</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a href="javascript:void(0)" class="collapsible-header"><i class="fa fa-user" aria-hidden="true"></i> Feedback details</a>
+                            <div class="collapsible-body left-sub-menu">
+                                <ul>
+                                    <li><a href="all_feedback.php">Feedback</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a href="javascript:void(0)" class="collapsible-header"><i class="fa fa-user" aria-hidden="true"></i> Enquiry Details</a>
+                            <div class="collapsible-body left-sub-menu">
+                                <ul>
+                                    <li><a href="users.php">Enquiry Details</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        
+                        
+                        <li style="height: 100%;"><a href="login.html" target="_blank"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+
+
+
+
+</body>
+</html>
